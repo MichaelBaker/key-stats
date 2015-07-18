0cab70e9749838856b3bb3a456bbe40767af1387
Michael Baker <michaeltbaker@gmail.com>
2015-07-18 13:10:32 -0500

    Adds a menu bar item that you can click to print text in the console.

--- KeyStats/AppDelegate.swift
+++ KeyStats/AppDelegate.swift
@@ -12,14 +12,26 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
+    
+    // -2 here is actually a constant defining the type of status item we want,
+    // but Swift doesn't recognize the constant name for whatever reason so I,
+    // had to use the number literal instead.
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-2)
 
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        if let button = statusItem.button {
+            button.image  = NSImage(named: "KeyStatusIcon")
+            button.action = Selector("printQuote:")
+        }
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
+    }
+    
+    func printQuote(sender: AnyObject) {
+        println("This is not an NSString")
     }
 
 
